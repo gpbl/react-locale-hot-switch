756ec14df15f909dbe6f13f60ba4d2608f11b8fa
Giampaolo Bellavite <io@gpbl.org>
2014-12-21 23:58:01 +0100

    Delete server.js

--- server.js
+++
@@ -1,4 +0,0 @@
-var path = require('path');
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(path.resolve(__dirname, 'public'))).listen(3000);
