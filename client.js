6d7d79db212132674966ad5397dd0c1d378f6ba6
Giampaolo Bellavite <io@gpblv.org>
2014-12-22 01:13:43 +0100

    Grab default locale from <html>

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
 var React = require('react');
 var i18nLoader = require('./i18n');
+var App = require('./App.js');
 
 function renderApp(i18n) {
-  var App = require('./App.js');
   React.render(<App {...i18n} />, document.body);
 }
 
-var defaultLocale = 'en';
+var defaultLocale = document.documentElement.getAttribute('lang');
 
 i18nLoader(defaultLocale, renderApp);
