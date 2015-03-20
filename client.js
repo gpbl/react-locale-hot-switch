441f6b8aad861b6333085a5df3ee839720624815
gpbl <io@gpbl.org>
2015-03-20 08:13:23 +0100

    Move App's require in the callback #2

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 var React = require('react');
 var i18nLoader = require('./i18n');
-var App = require('./App.js');
 
 function renderApp(i18n) {
+  var App = require('./App.js');
   React.render(<App {...i18n} />, document.body);
 }
 
